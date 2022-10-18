2ce5a55035758fdee72d2b2f2bfb1f47f5cba8f6
yousfa-naqvi <46033064+yousfa-naqvi@users.noreply.github.com>
2022-10-18 01:13:28 -0400

    final version 8c

--- server/index.js
+++ server/index.js
@@ -14,25 +14,7 @@ require('dotenv').config();
 const stripe= require("stripe")(process.env.stripkey);
 const app = exp();
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
-app.use(function (req, res, next) {
-
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://shoppingcart-fwl2dz7n1-yousfanaqvi.vercel.app');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
+app.use( cors({origin: ['https://shoppingcart-fwl2dz7n1-yousfanaqvi.vercel.app']}));
 app.use(exp.json());
 
 
