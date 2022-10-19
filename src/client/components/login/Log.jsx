bdff9ddbcf09bca895c62992a3ce40fd0e013914
yousfa-naqvi <46033064+yousfa-naqvi@users.noreply.github.com>
2022-10-19 09:51:57 -0400

    v1.1

--- src/client/components/login/Log.jsx
+++ src/client/components/login/Log.jsx
@@ -31,7 +31,7 @@ function Log() {
     
         }
         // process.env.REACT_APP_BACKEND_URL
-        return fetch(`/loginUser`,{
+        return fetch(process.env.REACT_APP_BACKEND_URL+`/loginUser`,{
           method:"POST",
           headers,
           body:JSON.stringify(data)
