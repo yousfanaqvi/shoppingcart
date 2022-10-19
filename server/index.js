005e4edf69e62b19fee7e91856bd48bb7ee130c2
yousfa-naqvi <46033064+yousfa-naqvi@users.noreply.github.com>
2022-10-19 00:59:27 -0400

    final version 8d

--- server/index.js
+++ server/index.js
@@ -17,7 +17,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use( cors());
 app.use(exp.json());
 
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
 
 const port = process.env.PORT || 3000;
 
