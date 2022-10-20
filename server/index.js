1ac3c02c0f21fd1921094cc4baa55ad74b9217ca
yousfa-naqvi <46033064+yousfa-naqvi@users.noreply.github.com>
2022-10-20 00:53:22 -0400

    v2.4

--- server/index.js
+++ server/index.js
@@ -3,19 +3,89 @@ const regFn = require("./registerDB")
 const loginFn = require("./login")
 var Register = require('./dbSchema.js');
 var Customer = require('./customerSchema.js');
-const cors= require("cors");
-const exp = require('express');
-const path= require("path");
 const bodyParser=require("body-parser");
 require('dotenv').config();
-app.use( cors());
+const express = require("express");
+const app = express();
+const path = require("path");
+const cors = require("cors");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+app.get("/api/test", (req, res) => {
+  res.send("test");
+});
+
+app.use(express.static(path.join(__dirname, "./build")));
+
+app.get("*", function (_, res) {
+  res.sendFile(
+    path.join(__dirname, "./build/index.html"),
+    function (err) {
+      if (err) {
+        res.status(500).send(err);
+      }
+    }
+  );
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server Running on port ${port}`));
+
+module.exports = app;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // if (process.env.NODE_ENV !== 'production') {
 //   require('dotenv').config({path: __dirname+'/.env'});
 // }
 const stripe= require("stripe")(process.env.stripkey);
-const app = exp();
-app.use(bodyParser.urlencoded({ extended: true }));
+// const app = exp();
+// app.use(bodyParser.urlencoded({ extended: true }));
 //app.use(createProxyMiddleware('/', { target: 'https://shoppingcart-beta.vercel.app', "changeOrigin": true }))
 // var whitelist = ['https://shoppingcart-fwl2dz7n1-yousfanaqvi.vercel.app', 'https://shoppingcart-beta.vercel.app']
 // var corsOptions = {
@@ -27,12 +97,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //     }
 //   }
 // }
-app.use(exp.json());
-app.get("/api/test", (req, res) => {
-  res.send("test");
-});
+// app.use(exp.json());
+// app.get("/api/test", (req, res) => {
+//   res.send("test");
+// });
 
-app.get("/api/getdata", (req, res) => {
+//app.get("/api/getdata", (req, res) => {
   //console.log(req.query.email);
   // Register.find({},function(err,result){
   //       if(err)
@@ -43,142 +113,142 @@ app.get("/api/getdata", (req, res) => {
   //       res.send("not found");
   //      }
   //   });
-    res.send("hello");
-  });
+  //   res.send("hello");
+  // });
 
  
 
- app.post("/api/loginUser", (req,res) => {
-  Register.findOne({email:req.body.email,password:req.body.password},function(err,result){
-    if(err)
-    console.log("error");
-    else if(result)
-    {
-      res.statusMessage = "found";
-      res.status(200).end();
+//  app.post("/api/loginUser", (req,res) => {
+//   Register.findOne({email:req.body.email,password:req.body.password},function(err,result){
+//     if(err)
+//     console.log("error");
+//     else if(result)
+//     {
+//       res.statusMessage = "found";
+//       res.status(200).end();
 
-    }
-    else{
-      res.statusMessage = "Not found";
-      res.status(400).end();
-    }
-      
-  });
-  
-});
-
-app.use(exp.static(path.join(__dirname,"build")));
-  app.get("*",function(req,res){
-     res.sendFile(path.join(__dirname, "build","/index.html"));
-  });
-  
-// app.get("*", function (_, res) {
-//   res.sendFile(
-//     path.join(__dirname, "build","/index.html"),
-//     function (err) {
-//       if (err) {
-//         res.status(500).send(err);
-//       }
 //     }
-//   );
+//     else{
+//       res.statusMessage = "Not found";
+//       res.status(400).end();
+//     }
+      
+//   });
+  
 // });
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-//   next();
-// });
 
-const port = process.env.PORT || 3000;
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(exp.static(path.join(__dirname,"build")));
-//   app.get('/*', (req, res) => {
-//     res.sendFile(path.join(__dirname, "build","/index.html"));  })
-// }
-//app.options('*', cors())
-//   app.use(exp.static(path.join(__dirname,"build")));
-//   app.get("/",function(req,res){
-//     // res.sendFile(path.join(__dirname, "build","/index.html"));
-//     res.send('heelo');
-//  });
+// app.use(exp.static(path.join(__dirname,"build")));
+//   app.get("*",function(req,res){
+//      res.sendFile(path.join(__dirname, "build","/index.html"));
+//   });
+  
+// // app.get("*", function (_, res) {
+// //   res.sendFile(
+// //     path.join(__dirname, "build","/index.html"),
+// //     function (err) {
+// //       if (err) {
+// //         res.status(500).send(err);
+// //       }
+// //     }
+// //   );
+// // });
+// // app.use(function(req, res, next) {
+// //   res.header("Access-Control-Allow-Origin", "*");
+// //   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+// //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+// //   next();
+// // });
+
+// const port = process.env.PORT || 3000;
+// // if (process.env.NODE_ENV === 'production') {
+// //   app.use(exp.static(path.join(__dirname,"build")));
+// //   app.get('/*', (req, res) => {
+// //     res.sendFile(path.join(__dirname, "build","/index.html"));  })
+// // }
+// //app.options('*', cors())
+// //   app.use(exp.static(path.join(__dirname,"build")));
+// //   app.get("/",function(req,res){
+// //     // res.sendFile(path.join(__dirname, "build","/index.html"));
+// //     res.send('heelo');
+// //  });
 
 
-//create a customer
+// //create a customer
 
-app.post("/payment" ,  (req,res) => {
-    const {product,token}=req.body;
-    console.log("product", product);
-    console.log("price", product.price);
+// app.post("/payment" ,  (req,res) => {
+//     const {product,token}=req.body;
+//     console.log("product", product);
+//     console.log("price", product.price);
     
 
-    return stripe.customers
-    .create({
-      email:token.email,
-      source:token.id
-    })
-    .then(customer =>{
-      stripe.charges.create({
-        amount:product.price,
-        currency:"usd",
-        customer:customer.id,
-        receipt_email:token.email,
-        description:`purchase of ${product.name}`,
+//     return stripe.customers
+//     .create({
+//       email:token.email,
+//       source:token.id
+//     })
+//     .then(customer =>{
+//       stripe.charges.create({
+//         amount:product.price,
+//         currency:"usd",
+//         customer:customer.id,
+//         receipt_email:token.email,
+//         description:`purchase of ${product.name}`,
         
-        shipping:{
-          name:token.card.name,
-          address:{
-            country:token.card.address_country
-          }
-        }
+//         shipping:{
+//           name:token.card.name,
+//           address:{
+//             country:token.card.address_country
+//           }
+//         }
 
-      })
-    })
-    .then(result => {
-      res.status(200).json(result);
+//       })
+//     })
+//     .then(result => {
+//       res.status(200).json(result);
       
-  }
-    )
-    .catch(err => console.log(err))
+//   }
+//     )
+//     .catch(err => console.log(err))
 
 
-})
-
-
-
-app.post("/storeData", (req,res) => {
-  const db=f.connectDB(req.body);
-});
+// })
 
 
 
-app.post("/registerUser",(req,res) => {
-  const reg=regFn.registerCustomer(req.body);
-  Register.find({email:req.body.email},function(err,result){
-    if(err)
-    console.log("error");
-    else if(result.length!==0)
-    {
-      res.statusMessage = "found";
-      res.status(200).end();
-      console.log(result);
-    }
-    else{
-      res.statusMessage = "Not found";
-      res.status(400).end();
+// app.post("/storeData", (req,res) => {
+//   const db=f.connectDB(req.body);
+// });
+
+
+
+// app.post("/registerUser",(req,res) => {
+//   const reg=regFn.registerCustomer(req.body);
+//   Register.find({email:req.body.email},function(err,result){
+//     if(err)
+//     console.log("error");
+//     else if(result.length!==0)
+//     {
+//       res.statusMessage = "found";
+//       res.status(200).end();
+//       console.log(result);
+//     }
+//     else{
+//       res.statusMessage = "Not found";
+//       res.status(400).end();
       
-    }
+//     }
       
-  });
+//   });
   
     
-});
+// });
 
 
 
-app.listen(port,function(err){
-    if(err)
-    console.log(err);
-    else
-    console.log("server is running.....");
+// app.listen(port,function(err){
+//     if(err)
+//     console.log(err);
+//     else
+//     console.log("server is running.....");
 
-});
+// });
