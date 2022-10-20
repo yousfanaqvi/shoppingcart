42eaa96fe7f3fb589ad430459553fc8d53c2b52e
yousfa-naqvi <46033064+yousfa-naqvi@users.noreply.github.com>
2022-10-19 21:43:49 -0400

    v2

--- server/index.js
+++ server/index.js
@@ -66,7 +66,7 @@ app.get("/api/getdata", (req, res) => {
 });
 
 app.use(exp.static(path.join(__dirname,"build")));
-  app.get("/",function(req,res){
+  app.get("*",function(req,res){
      res.sendFile(path.join(__dirname, "build","/index.html"));
   });
   
